9bfd196ab8ac8a95ba125e179dace556ec6b6274
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-12-20 04:06:31 -0300

    chore(refactor): Refactor prcl.IntWidth().
    Closes #1.

--- porcelana/bengala.go
+++ porcelana/bengala.go
@@ -15,20 +15,31 @@ import "reflect"
 /* Matches the integer width number for a
  * integer passed per reflect.Kind. */
 func IntWidth(reflectk reflect.Kind) int {
+	/* (u)int type-to-size map. */
+	int_sizes := map[reflect.Kind]int{
+		reflect.Uint8:  8,
+		reflect.Int8:   8,
+		reflect.Uint16: 16,
+		reflect.Int16:  16,
+		reflect.Uint32: 32,
+		reflect.Int32:  32,
+		reflect.Uint64: 64,
+		reflect.Int64:  64,
+	}
+
 	switch reflectk {
+	/* Determine generic (u)int size per platform. */
 	case reflect.Uint, reflect.Int:
 		var x int
 		return int(reflect.TypeOf(x).Size())
-	case reflect.Uint8, reflect.Int8:
-		return 8
-	case reflect.Uint16, reflect.Int16:
-		return 16
-	case reflect.Uint32, reflect.Int32:
-		return 32
-	case reflect.Uint64, reflect.Int64:
-		return 64
 	default:
 		break
 	}
+
+	/* Otherwise, verify the map. */
+	if size, isitevenint := int_sizes[reflectk]; isitevenint {
+		return size
+	}
+
 	return -1 /* Not an integer. */
 }
