12e2eb8be4187660df1f768f450c7e1823d1c6b3
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-08-20 00:43:48 -0300

    feat: Start writing initial code for parsing the header.

--- capio/parser.go
+++ capio/parser.go
@@ -16,48 +16,113 @@ import (
 	"fmt"
 	"os"
 	"pindorama.net.br/libcmon/bass"
+	"time"
 )
+
+/* nula is a null (\0) character. */
+var nula = byte(0)
 
 // whatHeaderIsIt does what its name implies: verifies what
 // the cpio header is. It correlates the magic number with
 // a internal constant for it, making it easier to deal with
 // each header type.
-func whatHeaderIsIt(file *os.File) (Format, error) {
-	buffer, _, err := bass.Walk(file, 512)
-	if err != nil {
-		return ZILTCH, err
-	}
-
+func whatHeaderIsIt(buffer []byte) Format {
 	/* First two bytes for the binary magic. */
 	switch MAGIC_BINARY {
 	case (binary.LittleEndian.Uint16(buffer[:2])):
-		return HEADER_BINLE, nil
+		return HEADER_BINLE
 	case (binary.BigEndian.Uint16(buffer[:2])):
-		return HEADER_BINBE, nil
+		return HEADER_BINBE
 	}
 
 	/* Modern ascii format and variations. */
 	if bytes.Equal(buffer[:6], MAGIC_ODC) {
-		fmt.Println("ODC")
-		return HEADER_ODC, nil
-	} else if bytes.Equal(buffer[:6], MAGIC_CRC) {
-		fmt.Println("CRC")
-		return HEADER_CRC, nil
-	} else if bytes.Equal(buffer[:6], MAGIC_ASCII) {
 		/* TODO: Special treatment for HEADER_DEC.
+		 * "The DEC format is rubbish." - Gunnar Ritter
 		 *
-			 * if (itsaDEC) {
+		 * if (itsaDEC) {
 		 *	return HEADER_DEC, nil
 		 * }
-		*/
-		return HEADER_ASC, nil
+		 */
+		return HEADER_ODC
+	} else if bytes.Equal(buffer[:6], MAGIC_CRC) {
+		return HEADER_CRC
+	} else if bytes.Equal(buffer[:6], MAGIC_ASCII) {
+		return HEADER_ASC
 	}
 
-	return ZILTCH, err
+	return ZILTCH
+}
+
+
+// whatsTheHeaderType determines the specific type of the archive,
+// besides whether it is binary (L.E. or B.E.) or ASCII/new cpio.
+/*
+ * func whatsTheHeaderType(buffer []byte) (error) {
+ *
+ *
+ * }
+ */
+
+/*
+ * func validateFile(file *os.File) {
+ * }
+ */
+
+func doTheParse(file *os.File) (*Header, error) {
+	header := []byte("")
+	entry := &Header{}
+	buffer, _, err := bass.Walk(file, 512)
+	var teste time.Time
+	if err != nil {
+		return nil, err
+	}
+	header_format := whatHeaderIsIt(buffer)
+	fmt.Println(header_format.String())
+
+	for i := 0; i < len(buffer); i += 2 {
+		if i == 0 {
+			continue
+		}
+		if (buffer[(i-1)] != nula) &&
+			bytes.Equal(buffer[i:(i+2)], []byte{nula, nula}) {
+			header = buffer[:(i + 2)]
+		}
+	}
+
+	switch (header_format & TYPE_BINARY) {
+	case 0: /* ASCII, ODC, CRC, etc. */
+		println("Isso é tudo, pe-pessoal!")
+		println("... Por ora.")
+	default: /* Binary, either little or big endian. */
+		entry = &Header{
+			Typeflag:   nula,
+			Name:       "",
+			Linkname:   "",
+			Size:       0,
+			Mode:       0,
+			Uid:        0,
+			Gid:        0,
+			Uname:      "",
+			Gname:      "",
+			ModTime:    teste,
+			AccessTime: teste,
+			ChangeTime: teste,
+			Devmajor:   0,
+			Devminor:   0,
+			Magic:      header[:2],
+			Format:     header_format,
+		}
+	}
+
+	fmt.Printf("%#v\n", header)
+	return entry, nil
 }
 
 func CallFromTest(file *os.File) {
-	header, _ := whatHeaderIsIt(file)
-	println((header & TYPE_BINARY))
-	println(header.String())
+	entry, err := doTheParse(file)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	fmt.Printf("%#v\n", entry)
 }
