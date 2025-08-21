e2acba11d5ccc461e318442eb4b9eda9bd9f1f06
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-08-20 22:17:38 -0300

    feat: Added bin_header.

--- capio/parser.go
+++ capio/parser.go
@@ -196,6 +196,18 @@ func doTheParse(file *os.File) (*Header, error) {
 		fallthrough
 	default:
 		magic = header[:2]
+		bin_header = rawBINHeader{
+			h_dev: header[2:4],
+			h_inode: header[4:6],
+			h_mode: header[6:8],
+			h_uid:header[8:10],
+			h_gid:header[10:12],
+			h_nlink:header[12:14],
+			h_majmin:header[14:16],
+			h_mtime:header[16:20],
+			h_namesize:header[20:22],
+			h_filesize:header[22:26],
+		}
 		switch header_format & TYPE_BE {
 		case 0: /* Binary, little endian. */
 			println("LE")
