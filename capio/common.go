a28defdc7bb68d272fbebce968a4be61017046e5
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2025-08-21 12:46:15 -0300

    chore: Added ErrChecksum to capio/common.go.

--- capio/common.go
+++ capio/common.go
@@ -1,11 +1,22 @@
+/*
+ * capio/format.go - Programmatical description of the cpio format
+ *
+ * Copyright (C) 2025: Pindorama
+ *		Luiz Antônio Rangel (takusuman)
+ *
+ * SPDX-Licence-Identifier: BSD-3-Clause
+ *
+ */
+
 package capio
 
 import "errors"
 
 var (
-	ErrHeader          = errors.New("capio: invalid header")
+	ErrHeader          = errors.New("capio: invalid header; perhaps corrupt")
 	ErrWriteTooLong    = errors.New("capio: write too long")
 	ErrFieldTooLong    = errors.New("capio: header field too long")
 	ErrWriteAfterClose = errors.New("capio: write after close")
 	ErrInsecurePath    = errors.New("capio: insecure file path")
+	ErrChecksum        = errors.New("capio: error computing checksum")
 )
